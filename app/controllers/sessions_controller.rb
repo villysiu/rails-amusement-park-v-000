efba19d48d88ea52fe918a7892296bf323c68fb8
villysiu <villysiu@gmail.com>
2021-05-28 11:26:24 -0700

    setup login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+  #params = {"user"=>{"name"=>"mickey", "password"=>"mouse"}, "commit"=>"log in", "controller"=>"sessions", "action"=>"create"}
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      log_in(@user)
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new' #, :flash => { :danger => 'Invalid email/password combination' }
+    end
   end
-
   def destroy
-  end
+    #session.delete :user_id
+    log_out
+    redirect_to '/'
+
+    end
 end
